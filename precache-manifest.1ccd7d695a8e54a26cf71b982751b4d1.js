5d889b6c500f3427df6ca1f206abf322955535fc
KO <ko@KOs-MacBook-Pro.local>
2020-08-06 22:40:26 +0000

    Updates

---
+++ precache-manifest.1ccd7d695a8e54a26cf71b982751b4d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbcf2ce05b8fee3062900e916c1e676",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "1f49924c18ce6260bb52",
+    "url": "/mycv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2842cb993326d9abd22a",
+    "url": "/mycv/static/js/2.4c143771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycv/static/js/2.4c143771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f49924c18ce6260bb52",
+    "url": "/mycv/static/js/main.6839ff54.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mycv/static/media/logo.5d5d9eef.svg"
+  }
+]);
